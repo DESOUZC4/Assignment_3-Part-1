1da311b11bbe521d97097a92b19c978275093458
DESOUZC4 <cdesouza.deakin.edu.au>
2020-06-03 14:49:36 +1000

    File Downloaded

---
+++ trial.R
@@ -0,0 +1,3 @@
+Assigment 3 Part 1
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/gene_expression.tsv", destfile = "gene.expression.tsv")
+x<- read.table("gene.expression.tsv")
